c3d4afc95b637b814a07b04829a49742ce20743d
Veronica Lopez <nicaa0695@gmail.com>
2019-08-28 17:15:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,22 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
     
-    get '/teams' do 
-      erb :team 
-    end 
-    
-    
+     get '/' do 
+      erb :super_hero
+    end
+
+     post '/teams' do
+     @team = Team.new(params[:team])
+
+      params[:team][:heroes].each do |args|
+       Hero.new(args)
+    end
+
+     @heroes = Hero.all 
+
+     erb :team
+
+   end
     
     
 
